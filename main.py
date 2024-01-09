b1eec8bd4364e87b01ade6990d0d7b39d6e79c43
gajdam <gajam11@gmail.com>
2024-01-09 18:58:35 +0100

    db added

--- main.py
+++ main.py
@@ -3,3 +3,25 @@ from pydantic import BaseModel
 import sqlite3
 
 app = FastAPI()
+
+
+conn = sqlite3.connect("north_pole.db")
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS elves (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        free BOOLEAN,
+        maternity_leave BOOLEAN
+    )
+''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS packages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        elf_id INTEGER,
+        FOREIGN KEY (elf_id) REFERENCES elves (id)
+    )
+''')
+conn.commit()
+conn.close()
